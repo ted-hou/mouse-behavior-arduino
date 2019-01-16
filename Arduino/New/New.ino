bce7f2f476cee752e526ca5ca89767a605699ac3
Julia <jknguyen@g.harvard.edu>
2019-01-16 11:48:42 -0500

    Proactive Pav in middle of window; only half window visible
    Trying to make circle stim into half moon...need to add rotation

--- Arduino/New/New.ino
+++ Arduino/New/New.ino
@@ -976,9 +976,17 @@ void state_response_window()
 	else if (_params[PAVLOVIAN] == 1)
 	{
 		if (_params[REACTIVE] == 0)
-		// deliver reward some time in this window from alpha to turning pt
 		{
-			if ((getTimeSinceStimOn() - _timeAlpha) >= randDelay)
+			if (_command == 'A')
+			{
+				_resultCode = CODE_PAVLOVIAN;
+				_state = STATE_REWARD;
+				return;
+			}
+		}
+		else if (_params[REACTIVE] == 1)
+		{
+			if (_command == 'T')
 			{
 				_resultCode = CODE_PAVLOVIAN;
 				_state = STATE_REWARD;
