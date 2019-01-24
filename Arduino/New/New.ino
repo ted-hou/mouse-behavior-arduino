0cc8033163a4044c3be7fc986e59bec81de937d1
Julia <jknguyen@g.harvard.edu>
2019-01-23 19:23:32 -0500

    Revert "Still working on the audio"
    This reverts commit e570f9c5e9e5c8f7f1041f745215621428a3f370.

--- Arduino/New/New.ino
+++ Arduino/New/New.ino
@@ -364,8 +364,6 @@ static bool _isLickOnset 					= false;		// True during lick onset
 static bool _firstMoveRegistered 			= false;		// True when first lick is registered for this trial
 static long _timeLastLick					= 0;			// Time (ms) when last lick occured
 
-static bool _toneRegistered					= false;
-
 static bool _isLeverPressed					= false;		// True as long as lever is pressed down
 static bool _isLeverPressOnset 				= false;		// True when lever first pressed
 static long _timeLastLeverPress				= 0;			// Time (ms) when last lever press (release) occured
@@ -399,6 +397,8 @@ void setup()
 	digitalWrite(4, LOW);
 	pinMode(5, OUTPUT);
 	digitalWrite(5, LOW);
+	pinMode(9, OUTPUT);
+	digitalWrite(9, LOW);
 	pinMode(10, OUTPUT);
 	digitalWrite(10, LOW);
 	pinMode(11, OUTPUT);
@@ -442,8 +442,6 @@ void mySetup()
 	_isLickOnset 					= false;		// True during lick onset
 	_firstMoveRegistered 			= false;		// True when first lick is registered for this trial
 	_timeLastLick					= 0;			// Time (ms) when last lick occured
-
-	_toneRegistered					= false;
 
 	_isLeverPressed					= false;		// True as long as lever is pressed down
 	_isLeverPressOnset 				= false;		// True when lever first pressed
@@ -648,7 +646,6 @@ void state_start()
 		// Reset variables
 		_resultCode = -1;
 		_firstMoveRegistered = false;
-		_toneRegistered = false;
 	}
 
 	/*****************************************************
@@ -978,10 +975,9 @@ void state_response_window()
 	{
 		if (_params[REACTIVE] == 0)
 		{
-			if (((getTimeSinceStimOn() - _timeAlpha) >= pavDelay) && !_toneRegistered)
+			if ((getTimeSinceStimOn() - _timeAlpha) >= pavDelay)
 			{	
 				playSound(TONE_CUE);
-				_toneRegistered = true;
 				sendEventMarker(EVENT_TONE_ON, -1);
 			}
 		}
