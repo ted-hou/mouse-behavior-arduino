ac2e414ddc15b31d62622a6c81a10c6ee07f7082
Lingfeng Hou <lingfenghou@g.harvard.edu>
2023-11-14 16:25:24 -0500

    Fixes

--- MATLAB/ThorlabsPowerMeter/autocalibrate.m
+++ MATLAB/ThorlabsPowerMeter/autocalibrate.m
@@ -2,26 +2,33 @@ hLaser = MouseBehaviorInterface('COM5'); % This is the laser control arduino
 hMotor = MouseBehaviorInterface('COM7'); % This is the stepping motor arduino (for moving mirror)
 
 %%
+try
+    meter.disconnect;                                      % Disconnect and release
+end
+
 clear p result
 p.mirrorPositions = [-300, 0];
-p.targetPowers = [0.1, 0.5] .* 1e-3;
+p.targetPowers = [0.5, 2, 6] .* 1e-3;
 p.wavelengths = [473, 593];
-p.stepDelays = [0.5, 1];
+p.stepDelays = [0.5, 4];
 p.aoutMin = 0;
 p.aoutMax = 4095;
-p.tolerancePercentage = 1e-2;
+p.tolerancePercentage = 2.5e-2;
 p.maxIters = 100;
-p.stepSizeMultiplier = 200000; % Step size = -multiplier * (read wattage - target wattage)
+p.maxStationaryIters = 5;
+% p.stepSizeMultiplier = 100000; % Step size = -multiplier * (read wattage - target wattage)
+p.stepSizeMultiplier = 10000; % Step size = -multiplier * (read wattage - target wattage)
 p.readThreshold = 60e-6; % Watts, values below this are considered zero.
-p.subthresholdStepSize = 500; % 
+% p.subthresholdStepSize = 500; % 
+p.subthresholdStepSize = 250; % 
 
 result.aoutValues = zeros(length(p.targetPowers), length(p.mirrorPositions), 2);
 result.targetReached = false(length(p.targetPowers), length(p.mirrorPositions), 2);
 result.powers = zeros(length(p.targetPowers), length(p.mirrorPositions), 2);
 
-meter_list=ThorlabsPowerMeter;                              % Initiate the meter_list
-DeviceDescription=meter_list.listdevices;               	% List available device(s)
-test_meter=meter_list.connect(DeviceDescription);           % Connect single/the first devices
+meterList=ThorlabsPowerMeter;                              % Initiate the meter_list
+DeviceDescription=meterList.listdevices;               	% List available device(s)
+meter=meterList.connect(DeviceDescription);           % Connect single/the first devices
 
 
 for iMirrorPos = 1:length(p.mirrorPositions)
@@ -31,56 +38,67 @@ for iMirrorPos = 1:length(p.mirrorPositions)
     end
     for iLaser = 1:2
         for iPower = 1:length(p.targetPowers)
-            test_meter.setWaveLength(p.wavelengths(iLaser));            % Set sensor wavelength
-            test_meter.setDispBrightness(0.3);                          % Set display brightness
-            test_meter.setAttenuation(0);                               % Set Attenuation
-            test_meter.sensorInfo;                                      % Retrive the sensor info
-            test_meter.setPowerAutoRange(1);                            % Set Autorange
+            meter.setWaveLength(p.wavelengths(iLaser));            % Set sensor wavelength
+            meter.setDispBrightness(0.3);                          % Set display brightness
+            meter.setAttenuation(0);                               % Set Attenuation
+            meter.sensorInfo;                                      % Retrive the sensor info
+            meter.setPowerAutoRange(1);                            % Set Autorange
             pause(5)                                                    % Pause the program a bit to allow the power meter to autoadjust
-            test_meter.setAverageTime(0.01);                            % Set average time for the measurement
-            test_meter.setTimeout(1000);                                % Set timeout value 
+            meter.setAverageTime(0.01);                            % Set average time for the measurement
+            meter.setTimeout(1000);                                % Set timeout value 
             
             hLaser.Arduino.SendMessage(sprintf('A %i %i', 1, 0));
             hLaser.Arduino.SendMessage(sprintf('A %i %i', 2, 0));
             
             aoutValue = 0;
+            aoutValueOld = 0;
             targetPwr = p.targetPowers(iPower);
             i = 0;
+            nStationaryIters = 0;
             
             while true
-                if i > p.maxIters
-                    warning('Failed to reach target after %i iterations. Target = %g, Value = %g', i, targetPwr, targetPwr + df)
+                if i > p.maxIters || nStationaryIters > p.maxStationaryIters
+                    warning('Failed to reach target after %i iterations. Target = %g, Value = %g', i, targetPwr, targetPwr + dPwr)
                     break
                 end
-            
-                test_meter.updateReading(p.stepDelays(iLaser));                          % Update the power reading(with interal period of 0.5s)
-                assert(test_meter.meterPowerUnit == 'W', 'Power meter unit is "%s" instead of "W"', test_meter.meterPowerUnit);
+
+                pause(p.stepDelays(iLaser));
+                meter.updateReading(0);                          % Update the power reading(with interal period of 0.5s)
+                assert(meter.meterPowerUnit == 'W', 'Power meter unit is "%s" instead of "W"', meter.meterPowerUnit);
             
                 i = i + 1;
-                df = test_meter.meterPowerReading - targetPwr;
-                if test_meter.meterPowerReading < p.readThreshold
-                    this_step_size = p.subthresholdStepSize;
+                dPwr = meter.meterPowerReading - targetPwr;
+                aoutValueOld = aoutValue;                
+                if meter.meterPowerReading < p.readThreshold
+                    thisStepSize = p.subthresholdStepSize;
                 else
-                    this_step_size = - ceil(p.stepSizeMultiplier * abs(df)) * sign(df);
+                    thisStepSize = - ceil(p.stepSizeMultiplier * abs(dPwr)) * sign(dPwr);
                 end
-                if abs(df) > p.tolerancePercentage * targetPwr
-                    aoutValue = aoutValue + this_step_size;
+                if abs(dPwr) > p.tolerancePercentage * targetPwr
+
+                    aoutValue = aoutValue + thisStepSize;
                     aoutValue = min(aoutValue, p.aoutMax);
                     aoutValue = max(aoutValue, p.aoutMin);
+                    dAout = aoutValue - aoutValueOld;
+                    if dAout == 0
+                        nStationaryIters = nStationaryIters + 1;
+                    else
+                        nStationaryIters = 0;
+                    end
                 else
                     fprintf('Target reached in %i iterations.\n', i)
-                    result.targetReached(iLaser, iPower, iMirrorPos) = true;
+                    result.targetReached(iPower, iMirrorPos, iLaser) = true;
                     break
                 end
                 hLaser.Arduino.SendMessage(sprintf('A %i %i', iLaser, aoutValue));
-                fprintf('df = %.3fmW, Setting AOUT %i to %i\n', df*1e3, iLaser, aoutValue)
+                fprintf('%gnm, i=%i, tgt=%.1fmW, df=%.3fmW, step=%i, AOUT(%i)=%i\n', p.wavelengths(iLaser), i, targetPwr*1e3, dPwr*1e3, thisStepSize, iLaser, aoutValue)
             
         %         fprintf('%.10f%c\r',test_meter.meterPowerReading,test_meter.meterPowerUnit);
             end
         
-            test_meter.updateReading(p.stepDelays(iLaser));
+            meter.updateReading(p.stepDelays(iLaser));
             result.aoutValues(iPower, iMirrorPos, iLaser) = aoutValue;
-            result.powers(iPower, iMirrorPos, iLaser) = test_meter.meterPowerReading;
+            result.powers(iPower, iMirrorPos, iLaser) = meter.meterPowerReading;
         end
     end
 end
@@ -89,6 +107,6 @@ end
 hLaser.Arduino.SendMessage(sprintf('A %i %i', 1, 0));
 hLaser.Arduino.SendMessage(sprintf('A %i %i', 2, 0));
 
-test_meter.disconnect;                                      % Disconnect and release
+meter.disconnect;                                      % Disconnect and release
 
 clearvars -except hLaser hMotor p result
