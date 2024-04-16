017e39b5c09860162e17c70b3d103f7eaa481dc1
Lingfeng Hou <lingfenghou@g.harvard.edu>
2024-04-16 18:05:43 -0400

    Update example_new.m

--- MATLAB/AutoOpto/example_new.m
+++ MATLAB/AutoOpto/example_new.m
@@ -8,25 +8,24 @@ exp.saveArduinoParams();
 
 %% Run calibration 
 % 0.5NA, 0.33NA, 0.16NA, 0NA
-% exp.calibrate(mirrorPositions=[-570, -380, -190, 0], ...
-%     targetPowers=[0.5, 2, 8].*1e-3, ...
-%     wavelengths=[473, 593], ...
-%     stepDelays=[0.5, 8], ...
-%     maxIters=64, ...
-%     maxStationaryIters=8, ...
-%     powerMeterThreshold=50e-6);
-% exp.validate(validationDelay=[1, 8])
+exp.calibrate(mirrorPositions=[-570, -380, -190, 0], ...
+    targetPowers=[0.025, 0.05, 0.1, 0.5, 2].*1e-3, ...
+    wavelengths=[473, 593], ...
+    stepDelays=[0.5, 8], ...
+    maxIters=64, ...
+    maxStationaryIters=4, ...
+    powerMeterThreshold=10e-6);
+exp.validate(validationDelay=[1, 8])
 
 %% Or load calibration
-
-sampleCalibration = load('calibration_20240403.mat');
-sampleCalibration = sampleCalibration.exp;
+sampleCalibration = load('C:\Users\Assad Lab\Dropbox (HMS)\Data\daisy19\daisy19_20240411_didnot_plugin_opticfiber\daisy19_20240411.mat');
+sampleCalibration = sampleCalibration.obj;
 
 exp.Params = sampleCalibration.Params;
 exp.Results = sampleCalibration.Results;
 
 %% Make stim/lever plan
-exp.planStim(nPulses=10, pulseWidth=0.01, ipi=0.5, preTrainDelay=8, postTrainDelay=1)
+exp.planStim(nPulses=10, pulseWidth=0.2, ipi=1, preTrainDelay=8, postTrainDelay=1)
 exp.planLever(nBlocksPerPosition=1, nPositions=2, randomize=true);
 exp.Plan.lever.positions = [2, 1];
 
@@ -42,10 +41,10 @@ exp.save();
 
 
 %% Finish residual stim conditions if we haven't gone through all the conditions
-exp.runStimSessionPlanned(residual=true, ignoreCompletion=false, iti=10);
+exp.runStimSessionPlanned(residual=true, ignoreCompletion=true, iti=10);
 
 %%
 exp.save();
 
 %% 
-exp.runStimSession(nPulses=10, pulseWidth=0.10, ipi=0.5, preTrainDelay=8, postTrainDelay=1, planned=false)
+exp.runStimSession(nPulses=10, pulseWidth=0.5, ipi=0.5, preTrainDelay=8, postTrainDelay=1, planned=false)
