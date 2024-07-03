a2fb6f9de3a57fc5c402f49d068cce15f58d2464
Lingfeng Hou <lingfenghou@g.harvard.edu>
2024-07-03 15:42:16 -0400

    Update example_SPN_excitability.m

--- MATLAB/AutoOpto/example_SPN_excitability.m
+++ MATLAB/AutoOpto/example_SPN_excitability.m
@@ -1,4 +1,7 @@
 %%
+% Optrode B along AP, facing right (lateral) of mouse
+
+%% 
 exp = TwoColorExperiment();
 
 %% Load params
@@ -10,12 +13,12 @@ exp.saveArduinoParams();
 % 0.33NA, 0NA
 exp.calibrate(mirrorPositions=[-380, 0], ...
     targetPowers=[0.050, 2].*1e-3, ...
-    wavelengths=[473], ...
-    stepDelays=[2], ...
+    wavelengths=[473, 593], ...
+    stepDelays=[2, 2], ...
     maxIters=64, ...
     maxStationaryIters=4, ...
     powerMeterThreshold=10e-6);
-exp.validate(validationDelay=[8])
+exp.validate(validationDelay=[8, 8])
 exp.save();
 %% Or load calibration
 % I saved it into daisy22 by mistake, copy over the calibration data but
@@ -39,28 +42,17 @@ exp.save();
 % exp.runStimSession(nPulses=10, pulseWidth=0.05, ipi=0.95, preTrainDelay=8, postTrainDelay=1, planned=false)
 % exp.runStimSession(nPulses=10, pulseWidth=0.01, ipi=0.99, preTrainDelay=8, postTrainDelay=1, planned=false)
 
-%% Give juice once every 10 stim trains
+%% Give juice once every 4 stim trains
 
-% 25uW, 10 trains per pos
-for i = 1:10
+% 25uW, 4 trains per pos
+for i = 1:4
     for iMirrorPos = 1:2
         exp.runStimTrain(iMirrorPos, 1, 1, nPulses=10, pulseWidth=0.200, ipi=0.8, preTrainDelay=5, postTrainDelay=1);
         pause(10);
     end
 end
 
-% Give juice:
-doReward(exp);
-
-% 25uW, 10 trains per pos
-for i = 1:10
-    for iMirrorPos = 1:2
-        exp.runStimTrain(iMirrorPos, 1, 1, nPulses=10, pulseWidth=0.200, ipi=0.8, preTrainDelay=5, postTrainDelay=1);
-        pause(10);
-    end
-end
-
-% 2mW, 10 trains per pos
+%% 2mW, 10 trains per pos
 for i = 1:10
     for iMirrorPos = 1:2
         exp.runStimTrain(iMirrorPos, 2, 1, nPulses=10, pulseWidth=0.200, ipi=0.8, preTrainDelay=5, postTrainDelay=1);
@@ -68,20 +60,36 @@ for i = 1:10
     end
 end
 
-% Give juice:
-% Do 10 trains per condition (only two conditions i.e. two mirror
-% positions)
+%% 25uW, 4 trains per pos
+for i = 1:4
+    for iMirrorPos = 1:2
+        exp.runStimTrain(iMirrorPos, 1, 1, nPulses=10, pulseWidth=0.200, ipi=0.8, preTrainDelay=5, postTrainDelay=1);
+        pause(10);
+    end
+end
+
+%% Give juice a lot, might do some manuals:
 doReward(exp);
 
+%% 25uW, 10 trains per pos
+for i = 1:5
+    for iMirrorPos = 1:2
+        exp.runStimTrain(iMirrorPos, 1, 1, nPulses=10, pulseWidth=0.200, ipi=0.8, preTrainDelay=5, postTrainDelay=1);
+        pause(10);
+    end
+end
+
 % 2mW, 10 trains per pos
-for i = 1:10
+for i = 1:5
     for iMirrorPos = 1:2
         exp.runStimTrain(iMirrorPos, 2, 1, nPulses=10, pulseWidth=0.200, ipi=0.8, preTrainDelay=5, postTrainDelay=1);
         pause(10);
     end
 end
-
-
+%%
+exp.runStimTrain(iMirrorPos, 2, 1, nPulses=20, pulseWidth=1, ipi=1, preTrainDelay=5, postTrainDelay=1);
+%%
+exp.runStimTrain(iMirrorPos, 1, 1, nPulses=20, pulseWidth=1, ipi=1, preTrainDelay=5, postTrainDelay=1);
 
 function doReward(exp)
     DEBUG = true;
