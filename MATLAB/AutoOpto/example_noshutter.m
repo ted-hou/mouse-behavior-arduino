8efde171a38d38778a2a5588cad4c4a17d346741
ted-hou <ted_hou@126.com>
2025-03-31 15:25:29 -0400

    Update example_noshutter.m

--- MATLAB/AutoOpto/example_noshutter.m
+++ MATLAB/AutoOpto/example_noshutter.m
@@ -3,22 +3,22 @@ exp = TwoColorExperiment();
 
 %% Load params
 exp.saveArduino();
-% exp.loadArduinoParams();
+exp.loadArduinoParams();
 exp.saveArduinoParams();
 
 %% Run calibration 
 % 0.5NA, 0.33NA, 0.16NA, 0NA
-% exp.calibrate(mirrorPositions=[-570, -380, -190, 0], ...
-%     targetPowers=[0.025, 0.05, 0.1, 0.5, 2].*1e-3, ...
-%     wavelengths=[473, 635], ...
-%     stepDelays=[0.5, 0.5], ...
-%     maxIters=64, ...
-%     maxStationaryIters=4, ...
-%     powerMeterThreshold=10e-6);
-% exp.validate(validationDelay=[1, 1])
+exp.calibrate(mirrorPositions=[-570, -380, -190, 0], ...
+    targetPowers=[0.025, 0.05, 0.1, 2].*1e-3, ... targetPowers=[0.025, 0.05, 0.1, 0.5, 2].*1e-3, ...
+    wavelengths=[473, 635], ...
+    stepDelays=[0.5, 0.5], ...
+    maxIters=64, ...
+    maxStationaryIters=4, ...
+    powerMeterThreshold=2*10e-6);
+exp.validate(validationDelay=[1, 1])
 
 %% Or load calibration
-% sampleCalibration = load('C:\Users\Assad Lab\Dropbox (HMS)\Data\daisy19\daisy19_20240411_didnot_plugin_opticfiber\daisy19_20240411.mat');
+% sampleCalibration = load('C:\Users\assad\Desktop\test\test_20250331.mat');
 % sampleCalibration = sampleCalibration.obj;
 % 
 % exp.Params = sampleCalibration.Params;
@@ -27,10 +27,12 @@ exp.saveArduinoParams();
 %% Make stim/lever plan
 % exp.planStim(nPulses=10, pulseWidth=0.05, ipi=0.95)
 exp.planTask(includeLever=true, includeLick=true, nBlocksPerTask=1, nPositions=2, randomize=false);
-exp.Plan.task.positions = [2, 0, 1]; % lever 1, lever 2, then lick
+exp.Plan.task.positions = [2, 0, 1]; % lever 1 (initial), lever 2, then lick
+exp.Plan.task.TIMEOUT_MAX = [10000, 10000, 10000];
 
 exp.save();
 
+% exp.LaserArduino.DebugMode = true;
 %% Start both arduinos and cameras
 % exp.MotorArduino.Start();
 % exp.LaserArduino.Start();
